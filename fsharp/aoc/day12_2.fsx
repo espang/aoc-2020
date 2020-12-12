78657b3b6eb58506373084af8d86c181d5c0fdc9
Eike Spang <eike.spang@gmail.com>
2020-12-12 09:21:26 +0000

    refactor day12 part2

--- fsharp/aoc/day12_2.fsx
+++ fsharp/aoc/day12_2.fsx
@@ -9,43 +9,6 @@ type Instruction =
     | Right
     | Forward
 
-type Ship =
-    { X: int
-      Y: int
-      WaypointX: int
-      WaypointY: int }
-
-let right (wx, wy) = wy, -wx
-
-let left (wx, wy) = -wy, wx
-
-let rec turn f s n =
-    if n < 0 then
-        failwith "expected to turn in multiples of 90"
-    else if n = 0 then
-        s
-    else
-        let (wx, wy) = f (s.WaypointX, s.WaypointY)
-        turn
-            f
-            { s with
-                  WaypointX = wx
-                  WaypointY = wy }
-            (n - 90)
-
-let rec moveShip s i n =
-    match i with
-    | North -> { s with WaypointY = s.WaypointY + n }
-    | West -> { s with WaypointX = s.WaypointX - n }
-    | South -> { s with WaypointY = s.WaypointY - n }
-    | East -> { s with WaypointX = s.WaypointX + n }
-    | Forward ->
-        let newX = s.X + n * s.WaypointX
-        let newY = s.Y + n * s.WaypointY
-        { s with X = newX; Y = newY }
-    | Left -> turn left s n
-    | Right -> turn right s n
-
 let instructionFrom c =
     match c with
     | 'N' -> North
@@ -61,17 +24,51 @@ let instructions =
     File.ReadAllLines "input_12.txt"
     |> Seq.map (fun x -> instructionFrom x.[0], int x.[1..])
 
-let rec applyInstructions ship instructions =
-    match instructions with
-    | (i, n) :: tail -> applyInstructions (moveShip ship i n) tail
-    | [] -> ship
+type Ship =
+    { X: int
+      Y: int
+      WaypointX: int
+      WaypointY: int }
+
+let applyTimes n f args =
+    List.fold (fun acc _ -> f acc) args [ 1 .. n ]
+
+let right s =
+    { s with
+          WaypointX = s.WaypointY
+          WaypointY = -s.WaypointX }
+
+let left s =
+    { s with
+          WaypointX = -s.WaypointY
+          WaypointY = s.WaypointX }
+
+let turn f s n =
+    if n % 90 <> 0 then
+        failwith "expected to turn in multiples of 90"
+    else
+        applyTimes (n/90) f s
+
+let moveShip s (i, n) =
+    match i with
+    | North -> { s with WaypointY = s.WaypointY + n }
+    | West -> { s with WaypointX = s.WaypointX - n }
+    | South -> { s with WaypointY = s.WaypointY - n }
+    | East -> { s with WaypointX = s.WaypointX + n }
+    | Forward ->
+        { s with
+              X = s.X + n * s.WaypointX
+              Y = s.Y + n * s.WaypointY }
+    | Left -> turn left s n
+    | Right -> turn right s n
 
 let ship =
-    applyInstructions
+    Seq.fold
+        moveShip
         { X = 0
           Y = 0
           WaypointX = 10
           WaypointY = 1 }
-        (Seq.toList instructions)
+        instructions
 
 abs ship.X + abs ship.Y
