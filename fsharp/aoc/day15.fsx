2aa055b787db5a76f1fb436a9e37108bed8a9a83
Eike Spang <eike.spang@gmail.com>
2020-12-15 13:25:35 +0000

    day15

---
+++ fsharp/aoc/day15.fsx
@@ -0,0 +1,27 @@
+open System.Collections.Generic
+
+let nthNumberSpoken n (numbers:int list) =
+    let lastIndex = Dictionary()
+    let add number time =
+        lastIndex.[number] <- time
+
+    let rec speak step lastNbr =
+        if step > n
+        then lastNbr
+        else
+            if lastIndex.ContainsKey lastNbr
+            then
+                let next = step - 1 - lastIndex.[lastNbr]
+                add lastNbr (step - 1)
+                speak (step + 1) next
+            else
+                add lastNbr (step - 1) 
+                speak (step + 1) 0
+    for index, number in List.indexed numbers do
+        add number (index + 1)
+    speak (1 + numbers.Length) (List.last numbers)
+
+let input = [ 5; 2; 8; 16; 18; 0; 1 ]
+
+printfn "part1: %d" (nthNumberSpoken 2020 input)
+printfn "part2: %d" (nthNumberSpoken 30000000 input)
