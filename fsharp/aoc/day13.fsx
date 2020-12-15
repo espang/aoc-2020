9d4152f3fe2a3d0b5978f96042ee4a83c07ec38d
Eike Spang <eike.spang@gmail.com>
2020-12-15 13:26:32 +0000

    format files

--- fsharp/aoc/day13.fsx
+++ fsharp/aoc/day13.fsx
@@ -3,50 +3,47 @@ open System.IO
 let earliestTime = 1000417
 
 let firstAfter busId earliestTime =
-    if earliestTime % busId = 0
-    then earliestTime
-    else ((earliestTime / busId) + 1) * busId 
+    if earliestTime % busId = 0 then earliestTime else ((earliestTime / busId) + 1) * busId
 
-let scoreForPart1 earliestTime (busId, departAt) =
-    busId * (departAt - earliestTime)
+let scoreForPart1 earliestTime (busId, departAt) = busId * (departAt - earliestTime)
 
-(Seq.map (int >> (fun x -> x, firstAfter x earliestTime)) ((File.ReadAllText "input_13.txt").Split ","
-|> Seq.filter (fun x -> x <> "x")))
+(Seq.map
+    (int >> (fun x -> x, firstAfter x earliestTime))
+     ((File.ReadAllText "input_13.txt").Split ","
+      |> Seq.filter (fun x -> x <> "x")))
 |> Seq.sortBy snd
 |> Seq.head
 |> scoreForPart1 earliestTime
 
-let findFirst (divider:bigint) (rem:bigint) (factor:bigint)=
+let findFirst (divider: bigint) (rem: bigint) (factor: bigint) =
     let rec loop f2 =
         let rem' = factor * f2 % divider
-        if rem' = rem
-        then f2
-        else
-        if rem' > factor
-        then
-            if rem' > rem 
-            then
+        if rem' = rem then
+            f2
+        else if rem' > factor then
+            if rem' > rem then
                 //jump to the divider
                 let jump = (divider - rem') / factor
                 loop (f2 + if jump > bigint 0 then jump else bigint 1)
-            else if rem / rem' > (bigint 1)
-            then
+            else if rem / rem' > (bigint 1) then
                 // jump to the remainder
-                let jump = (rem - rem') / factor 
+                let jump = (rem - rem') / factor
                 loop (f2 + if jump > bigint 0 then jump else bigint 1)
             else
                 loop (f2 + (bigint 1))
         else
             loop (f2 + (bigint 1))
+
     loop (bigint 1)
 
-let findTimestamp (busIds:(bigint*bigint) list) =
+let findTimestamp (busIds: (bigint * bigint) list) =
     let rec loop (b, m) busIds =
         match busIds with
-        | (b',m')::tail ->
+        | (b', m') :: tail ->
             let b2 = findFirst m (b + b') m'
             loop (b2 * m' - b', m' * m) tail
         | [] -> b
+
     loop busIds.[0] busIds.[1..]
 
 (File.ReadAllText "input_13.txt").Split ","
--- fsharp/aoc/day14.fsx
+++ fsharp/aoc/day14.fsx
@@ -1,15 +1,10 @@
 open System.IO
 open System.Text.RegularExpressions
 
-let numberToDecimalString (x:int) =
-    System.Convert.ToString(int64 x, 2)    
+let numberToDecimalString (x: int) = System.Convert.ToString(int64 x, 2)
 
-let numberExtend l (s:string) =
-    if s.Length < l
-    then    
-        String.replicate (l - s.Length) "0"
-        + s
-    else s
+let numberExtend l (s: string) =
+    if s.Length < l then String.replicate (l - s.Length) "0" + s else s
 
 let applyBitmask bitmask value =
     let value' =
@@ -19,36 +14,37 @@ let applyBitmask bitmask value =
         |> Seq.toList
         |> List.zip (Seq.toList bitmask)
         |> List.map (fun (bm, digit) ->
-                        match bm with
-                        | 'X' -> digit
-                        | _ -> bm)
+            match bm with
+            | 'X' -> digit
+            | _ -> bm)
         |> Array.ofList
         |> System.String.Concat
-    
+
     System.Convert.ToUInt64(value', 2)
 
-let lines =
-    File.ReadAllLines "input_14.txt"
+let lines = File.ReadAllLines "input_14.txt"
 
-let parseLine (line:string) =
+let parseLine (line: string) =
     let items = line.Split()
     let number = Regex "mem\[(\d*)\]"
-    if items.[0] = "mask"
-    then
+    if items.[0] = "mask" then
         true, 0, items.[2].Trim()
     else
-        let v = (number.Match items.[0]).Groups.[1].Value
+        let v =
+            (number.Match items.[0]).Groups.[1].Value
+
         false, int v, items.[2].Trim()
 
 let handleAllLines lines =
     let mutable mem = Map.empty
+
     let rec loop bm lines =
         match lines with
-        | l::tail ->
+        | l :: tail ->
             match parseLine l with
             | true, _, bm -> loop bm tail
             | false, pos, value ->
-              
+
                 let value' = applyBitmask bm (int value)
                 mem <- mem.Add(pos, value')
                 loop bm tail
@@ -62,40 +58,45 @@ let memory = handleAllLines (Array.toList lines)
 //part 1
 Map.fold (fun acc k v -> acc + v) (uint64 0) memory
 
-let addresses (bm:string) address =
+let addresses (bm: string) address =
     let bitmaskLength = bm.Length
     let mutable memAddrs = Set.empty
+
     let rec loop acc bm =
         match bm with
-        | ('X', _)::tail ->
-            loop ('1'::acc) tail
-            loop ('0'::acc) tail
-        | ('0', original)::tail -> loop (original::acc) tail
-        | ('1', _)::tail -> loop ('1'::acc) tail
-        | head::tail -> failwithf "unexpected tuple %A" head
+        | ('X', _) :: tail ->
+            loop ('1' :: acc) tail
+            loop ('0' :: acc) tail
+        | ('0', original) :: tail -> loop (original :: acc) tail
+        | ('1', _) :: tail -> loop ('1' :: acc) tail
+        | head :: tail -> failwithf "unexpected tuple %A" head
         | [] ->
             let value' =
                 List.rev acc
                 |> Array.ofList
                 |> System.String.Concat
+
             memAddrs <- memAddrs.Add(System.Convert.ToUInt64(value', 2))
 
-    loop [] (address
-             |> numberToDecimalString
-             |> numberExtend bitmaskLength
-             |> Seq.toList
-             |> List.zip (Seq.toList bm))
+    loop
+        []
+        (address
+         |> numberToDecimalString
+         |> numberExtend bitmaskLength
+         |> Seq.toList
+         |> List.zip (Seq.toList bm))
     memAddrs
 
 let handleAllLines2 lines =
     let mutable mem = Map.empty
+
     let rec loop bm lines =
         match lines with
-        | l::tail ->
+        | l :: tail ->
             match parseLine l with
             | true, _, bm -> loop bm tail
             | false, pos, value ->
-              
+
                 let newAddresses = addresses bm (int pos)
                 for addr in newAddresses do
                     mem <- mem.Add(addr, uint64 value)
